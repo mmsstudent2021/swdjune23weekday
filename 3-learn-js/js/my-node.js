107c3c193f353b06fff97e115471ae6ca821f564
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-08-25 10:51:37 +0630

    update css

---
+++ 3-learn-js/js/my-node.js
@@ -0,0 +1,15 @@
+const x = 5;
+const y = 10;
+
+const arr = ["a","b","c","d"];
+for(el of arr){
+    console.log(el);
+}
+
+function run(x,y){
+    return x + y;
+}
+
+console.log(run(3,4));
+
+// console.log(arr);
