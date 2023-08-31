df0af00931bb79051475c1505990cfcc65e8ef94
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-08-31 10:52:56 +0630

    dark mode

---
+++ 5-tailwindcss/tailwind-cli/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    fontFamily : {
+      'poppins' : ['Poppins','san'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
