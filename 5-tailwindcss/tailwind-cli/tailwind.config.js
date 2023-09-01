d57222df63bdef14298ef43b65f043f1a0c6ce81
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-09-01 09:58:48 +0630

    config colors

--- 5-tailwindcss/tailwind-cli/tailwind.config.js
+++ 5-tailwindcss/tailwind-cli/tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/*.{html,js}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    fontFamily : {
-      'poppins' : ['Poppins','san'],
+    fontFamily: {
+      poppins: ["Poppins", "san"],
     },
-    extend: {},
+
+    extend: {
+      colors: {
+        primary: {
+          100: "#dad5fd",
+          200: "#b6acfc",
+          300: "#9182fa",
+          400: "#6d59f9",
+          500: "#482ff7",
+          600: "#3a26c6",
+          700: "#2b1c94",
+          800: "#1d1363",
+          900: "#0e0931",
+        },
+        secondary: "#9c1de7",
+      },
+    },
   },
   plugins: [],
-}
-
+};
