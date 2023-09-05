57ca01933cf0bc2ea27ce0a22132870d22474a53
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-09-05 13:17:39 +0630

    final

--- 5-tailwindcss/tailwind-responsive/tailwind.config.js
+++ 5-tailwindcss/tailwind-responsive/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "primary" : "#2d6cdf",
+        "secondary" : "#46c3db",
+      },
+    },
   },
   plugins: [],
 }
