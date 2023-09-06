b32739882d4726a5a07f92946eba8459b3137596
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-09-06 10:42:57 +0630

    config folders

--- 5-tailwindcss/tailwind-responsive/tailwind.config.js
+++ 5-tailwindcss/tailwind-responsive/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-    content: ["./src/*.{html,js}",],
+    content: [
+      "./src/*.{html,js}",
+      "./src/**/*.{html,js}",
+      "./src/**/**/*.{html,js}",
+  ],
   
   darkMode: 'class',
   theme: {
@@ -11,8 +15,9 @@ module.exports = {
         "secondary" : "#46c3db",
       },
       fontFamily: {
-        'open-sans': ['Open Sans','sans-serif'],
-        'roboto' : ['Roboto', 'sans-serif'],
+        'open-sans': ['Open Sans','Padauk'],
+        'roboto' : ['Roboto', 'Padauk'],
+        'padauk' : ['Padauk', 'sans-serif']
       },
 
     },
