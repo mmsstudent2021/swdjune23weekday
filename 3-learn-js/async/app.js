9b703b76cdd0ca914e2b9b76b23ae15ef3e7d3c1
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-11-28 10:08:58 +0630

    json server start

---
+++ 3-learn-js/async/app.js
@@ -0,0 +1,69 @@
+// sync & async
+
+// 
+
+
+// fetch("https://fakestoreapi.com/products/1")
+// .then(res => res.json())
+// .then(json => console.log(json))
+
+// fetch("https://fakestoreapi.com/products/2")
+// .then(res => res.json())
+// .then(json => console.log(json))
+
+// fetch("https://fakestoreapi.com/products/3")
+// .then(res => res.json())
+// .then(json => console.log(json))
+
+// fetch("https://fakestoreapi.com/products/4")
+// .then(res => res.json())
+// .then(json => console.log(json))
+
+// fetch("https://fakestoreapi.com/products/5")
+// .then(res => res.json())
+// .then(json => console.log(json))
+
+
+// console.log("one");
+// console.log("two");
+// console.log("three");
+
+// console.log("start");
+// let total = 0;
+// for(let x=1;x<999999999;x++){
+//     total += x;
+// }
+// console.log("total",total);
+// console.log("end");
+
+
+// async await
+
+// async function run(){
+//     const res1 = await fetch("https://fakestoreapi.com/products/1");
+//     const json1 = await res1.json();
+//     console.log(json1);
+
+//     const res2 = await fetch("https://fakestoreapi.com/products/2");
+//     const json2 = await res2.json();
+//     console.log(json2);
+
+//     const res3 = await fetch("https://fakestoreapi.com/products/3");
+//     const json3 = await res3.json();
+//     console.log(json3);
+
+//     const res4 = await fetch("https://fakestoreapi.com/products/4");
+//     const json4 = await res4.json();
+//     console.log(json4);
+
+//     const res5 = await fetch("https://fakestoreapi.com/products/5");
+//     const json5 = await res5.json();
+//     console.log(json5);
+// }
+
+// console.log(run());
+
+
+// data fetching
+
+// frontend => data => backend
---
+++ 3-learn-js/async/test.php
@@ -0,0 +1,15 @@
+<?php
+
+echo file_get_contents("https://fakestoreapi.com/products/1");
+echo "\n\n";
+echo file_get_contents("https://fakestoreapi.com/products/2");
+echo "\n\n";
+
+echo file_get_contents("https://fakestoreapi.com/products/3");
+echo "\n\n";
+
+echo file_get_contents("https://fakestoreapi.com/products/4");
+echo "\n\n";
+
+echo file_get_contents("https://fakestoreapi.com/products/5");
+echo "\n\n";
