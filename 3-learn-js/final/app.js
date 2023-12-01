3f7a61eebb7dcfea8ecdb044fdef073141875ec6
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-12-01 11:16:50 +0630

    up

---
+++ 3-learn-js/final/app.js
@@ -0,0 +1,40 @@
+// 1994-07-22
+// function findBirthYear(bd){
+//     const bdDate = new Date(bd);
+//     return bdDate.getFullYear();
+// }
+
+// const myName = "hein htet zan";
+// const myBd = "1994-07-22";
+
+// function findAge(birthYear){
+//     const date = new Date();
+//     return date.getFullYear() - birthYear;
+// }
+
+// let x = 10;
+// y = 15;
+
+// function intro(name,bd){
+//     const age = findAge(findBirthYear(bd));
+//     return `I'm ${name} and ${age} year${age > 1 ? "s" : ""} old`
+// }
+
+// console.log(intro(myName,myBd));
+
+
+
+// console.log("start");
+// let total = 0;
+// for(let i=1;i<999999999;i++){
+//     total+=i
+// }
+// console.log(total);
+// console.log("end");
+
+// console.log("start");
+// setTimeout(() => {
+//     console.log("mid");
+// },1000)
+
+// console.log("end");
